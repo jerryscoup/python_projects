7d65594d84185e575a57668c253128169b43ed7f
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-12 17:06:01 +0530

    Add files via upload

---
+++ 5x+3=2y_linear_equation_solving_program.py
@@ -0,0 +1,7 @@
+"""coded on 12 aug by jerryscoup
+for my our simplicity 
+equation is 5x+3=2y"""
+print("equation : 5x+3=2y")
+x=int(input("enter the value for x :"))
+y=((5*x)+3)/2
+print("if x is ",x,"then the value of y is :",y)
