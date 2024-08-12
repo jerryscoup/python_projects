1c0f5edd54cee04b9ab45d36369ad4662147211d
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-12 17:05:08 +0530

    Add files via upload

---
+++ calculating_perimeter_of_the_triangle.py
@@ -0,0 +1,12 @@
+"""written on aug 12 by puskarpuneet/jerryscoup
+purpose to input sides from user and to calculate perimetr of the triangle
+s1=side1
+s2=side2
+s3=side3
+peri=perimetre of the triangle"""
+print("enter the values of the three sides of the triangle:")
+s1=int(input("enter the value of side1:"))
+s2=int(input("enter the value of side2:"))
+s3=int(input("enter the value of side3:"))
+print("the perimeter of the triangle is:",s1+s2+s3)
+
