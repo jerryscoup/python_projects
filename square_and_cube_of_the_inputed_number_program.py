ef9b6eaabc85ddf259fd0d3a72599aeaf646d8fe
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-15 16:36:48 +0530

    Add files via upload

---
+++ square_and_cube_of_the_inputed_number_program.py
@@ -0,0 +1,5 @@
+"""written by jerryscoup
+purpose of the program is to output the square and cube of the inputed number"""
+print("squares and cubes of the number:")
+x=int(input("enter a number of which you want to find square and cube"))
+print("The square and cube of the number",x,"is :",x**2,",",x**3)
