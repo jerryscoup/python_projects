e9134c2eb2b0c9bece9f080540d2a125010fabdd
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-15 16:35:47 +0530

    Add files via upload

---
+++ find_the_remainder.py
@@ -0,0 +1,6 @@
+#written by jerryscoup
+# purpose of the code is to find the remainder if a number a is divided by another number b
+print("to divide a number a by other number b and hence find remainder of it")
+a=int(input("enter  value of the number a which should be the numerator "))
+b=int(input("enter the value of b which would be denominator"))
+print("the remainder of ",a,"/",b,"is :",a%b)
