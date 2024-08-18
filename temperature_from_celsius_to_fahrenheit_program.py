705a937d8803db797648c9f99e8bc248e01982f0
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-18 18:57:34 +0530

    Add files via upload

---
+++ temperature_from_celsius_to_fahrenheit_program.py
@@ -0,0 +1,4 @@
+print("program to convert temperature from celsius to fahrenheit")
+c=float(input("please enter the temperature in celsius:"))
+f=(9/5)*c+32
+print("the given inputed temperature in fahrenheit is :",f)
