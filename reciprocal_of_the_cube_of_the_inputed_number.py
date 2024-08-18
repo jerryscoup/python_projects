453eccb5f5b74f968c0d0abe635d41de98f2499f
prince/jerry_scoup <workforpuskarpuneet@gmail.com>
2024-08-18 18:57:12 +0530

    Add files via upload

---
+++ reciprocal_of_the_cube_of_the_inputed_number.py
@@ -0,0 +1,4 @@
+print("program to make reciprocalof cube of the number ")
+x=float(input("please enter the number any number: "))
+print("the cube of the inputed number is:",x**3)
+print("the reciprocal of the number",x**3,"is",1/(x**3))
